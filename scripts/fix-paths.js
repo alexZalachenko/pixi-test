3304a2f69904df5c22b51b15b363de85a5839422
alexZalachenko <alexverdumiralles@gmail.com>
2025-06-26 12:19:08 +0200

    Reverted the script for fixing dynamic imports

--- scripts/fix-paths.js
+++ scripts/fix-paths.js
@@ -9,10 +9,10 @@ fs.readdirSync(distPath).forEach(file => {
   // Only match files like index-*.js, index.something.js, etc.
   if (/^index.*\.js$/.test(file)) {
     let content = fs.readFileSync(filePath, 'utf-8');
-
+    
     // Replace any string like "assets/foo.js" with "./foo.js"
     content = content.replace(/"assets\//g, `"./`);
-
+    
     fs.writeFileSync(filePath, content);
     return;
   }
