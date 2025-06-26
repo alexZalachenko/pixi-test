4813a3baa4f16c158cbce15f44a16efbfef8c432
alexZalachenko <alexverdumiralles@gmail.com>
2025-06-26 12:03:48 +0200

    One more try to fix paths with dynamic imports.

--- scripts/fix-paths.js
+++ scripts/fix-paths.js
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 
 const distPath = './docs/assets';
-const base = '/pixi-test/';
 
 fs.readdirSync(distPath).forEach(file => {
   const filePath = path.join(distPath, file);
@@ -11,8 +10,8 @@ fs.readdirSync(distPath).forEach(file => {
   if (/^index.*\.js$/.test(file)) {
     let content = fs.readFileSync(filePath, 'utf-8');
 
-    // Replace any string like "assets/foo.js" with "/pixi-test/assets/foo.js"
-    content = content.replace(/"assets\//g, `"${base}assets/`);
+    // Replace any string like "assets/foo.js" with "./foo.js"
+    content = content.replace(/"assets\//g, `"./`);
 
     fs.writeFileSync(filePath, content);
     return;
